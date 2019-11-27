d69b45638ac701d1a1e8813e9d87f9a7bed5fc0b
Thiago do Nascimento <thiago@desenvolvedor.tech>
2019-11-27 15:21:10 -0300

    Models

--- backend/sala/admin.py
+++ backend/sala/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Sala
+from .models import Sala, SalaUsuario, SalaBanido
 # Register your models here.
 
 admin.site.register(Sala)
+admin.site.register(SalaUsuario)
+admin.site.register(SalaBanido)
